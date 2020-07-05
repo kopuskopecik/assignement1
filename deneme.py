db2e839d365c9918b5e9be86610ca37220511450
sahinturk <kopuskopecik@gmail.com>
2020-07-06 00:23:46 +0300

    My python file is created.

---
+++ deneme.py
@@ -0,0 +1,6 @@
+name = input("Please enter your name: ")
+
+if name == "Erdoğan":
+    print("Hello, {}! The password is : W@12".format(name))
+else:
+    print("Hello, {}! See you later.".format(name))
